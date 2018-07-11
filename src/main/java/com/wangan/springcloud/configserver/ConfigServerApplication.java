72e65417d4a4a5208916767d781eec5705d3448c
wa <15155313869@163.com>
2018-07-11 17:56:27 +0800

    Initial commit

---
+++ src/main/java/com/wangan/springcloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.wangan.springcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+//		System.out.println(System.getProperty("user.dir"));
+	}
+}
